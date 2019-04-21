e0c9df84d08cbe9c17cc775c2c9525e7b8413a58
劉育維 <simon3458@gmail.com>
2019-04-21 22:06:05 +0800

    Add files via upload

---
+++ AlexNet.py
@@ -0,0 +1,40 @@
+import numpy as np
+import keras
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten,Dropout
+from keras.optimizers import Adam
+
+#Build AlexNet model
+def AlexNet(width, height, depth, classes):
+    
+    model = Sequential()
+    
+    #First Convolution and Pooling layer
+    model.add(Conv2D(96,(11,11),strides=(4,4),input_shape=(width,height,depth),padding='valid',activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+    
+    #Second Convolution and Pooling layer
+    model.add(Conv2D(256,(5,5),strides=(1,1),padding='same',activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+    
+    #Three Convolution layer and Pooling Layer
+    model.add(Conv2D(384,(3,3),strides=(1,1),padding='same',activation='relu'))
+    model.add(Conv2D(384,(3,3),strides=(1,1),padding='same',activation='relu'))
+    model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+    
+    #Fully connection layer
+    model.add(Flatten())
+    model.add(Dense(4096,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000,activation='relu'))
+    model.add(Dropout(0.5))
+    
+    #Classfication layer
+    model.add(Dense(classes,activation='softmax'))
+
+    return model
